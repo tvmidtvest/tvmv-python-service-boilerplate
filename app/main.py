fa5515cb7c968744dfaa042d1715821c5720f70a
orvad <martin@metalab.dk>
2023-04-29 13:03:22 +0200

    Example variable

--- app/main.py
+++ app/main.py
@@ -1,19 +1,24 @@
+import os
+
 import config
 
 # Put this in all files that use the logger
 logger = config.get_logger(__name__)
 
 
-def example_function() -> None:
+def example_function(my_var: str) -> None:
     """ Example function."""
 
-    logger.info("Example function called")
+    logger.info(f"Example function called: {my_var}")
 
 
 def main() -> None:
     """ Main function."""
 
-    example_function()
+    # Get an environment variable
+    my_var = os.environ.get("MY_VAR")
+
+    example_function(my_var)
 
 
 if __name__ == '__main__':
